0d8555fdbe00277a7013e2eb9fd12a4c836dc6a7
matheusfillipe <matheusfillipeag@gmail.com>
2022-04-16 01:47:59 -0300

    initial commit

---
+++ build-site.el
@@ -0,0 +1,41 @@
+(require 'ox-publish)
+
+;; Org Publish
+(setq org-publish-use-timestamps-flag nil)
+(setq org-export-time-stamp-file nil)
+(setq org-export-with-broken-links t)
+(setq org-export-with-date t)
+(setq org-html-validation-link nil)
+(setq org-export-html-validation-link nil)
+
+(defun org-export-collect-headlines (info &optional n scope)
+  (let ((limit (plist-get info
+                          :headline-levels)))
+    (setq n (if (wholenump n)
+                (min n limit) limit))
+    (org-element-map (plist-get info
+                                :parse-tree) 'headline
+      #'(lambda (headline)
+         (unless (or (org-element-property :NOTOC headline) ; new condition
+                    (org-element-property :footnote-section-p headline)) ; old condition
+           (let ((level (org-export-get-relative-level headline info)))
+             (and (<= level n)
+                  headline))))
+      info)))
+
+(setq org-publish-project-alist '(("mattf.tk" :base-directory "./."
+                                   :base-extension "org"
+                                   :publishing-directory "~/Projects/sciblog/html/"
+                                   :with-creator t
+                                   :with-timestamps nil
+                                   :validate nil
+                                   :auto-sitemap t
+                                   :recursive t
+                                   :exclude "template.org"
+                                   :publishing-function org-html-publish-to-html
+                                   :headline-levels 4
+                                   :auto-preamble t)))
+
+(org-publish-all t)
+
+(message "Build complete!")
---
+++ build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+get_org_attr(){
+  file="$1"
+  attr="$2"
+  output=$(grep "#+$attr: " < "$file")
+  output=$(echo "$output" | sed -e "s/^#+$attr: //")
+  echo "$output"
+}
+
+gen_index() {
+  cd "$(dirname "$1")" || exit
+  readarray -d '' entries < <(printf '%s\0' *.org | sort -zV)
+  for entry in "${entries[@]}"; do
+    if [[ "$entry" == "index.org" ]]; then
+      continue
+    fi
+    title=$(get_org_attr "$entry" "TITLE")
+    description=$(get_org_attr "$entry" "DESCRIPTION")
+    escaped=$(printf '%s\n' "- [[file:$entry][$title]]       $description" | sed -e 's/[]\/$*.^[]/\\&/g')
+    list+="$escaped\n\n"
+  done
+  sed -e '1h;2,$H;$!d;g' -i -e "s/\(# LIST BEGIN\s*\n\).*\(# LIST END\s*\n\)/\1$list\2/ig" index.org
+  cd ..
+}
+
+gen_indexes() {
+  cd series/ || exit
+  series_list=""
+  for index in */index.org
+  do
+    title=$(get_org_attr "$index" "TITLE")
+    description=$(get_org_attr "$index" "DESCRIPTION")
+    gen_index "$index"
+    if [ -z "$title" ]; then
+      echo "No title found for index: $index"
+      continue
+    else
+      escaped=$(printf '%s\n' "- [[file:$index][$title]]" | sed -e 's/[]\/$*.^[]/\\&/g')
+      series_list+="$escaped\n\n"
+    fi
+  done
+  sed -e '1h;2,$H;$!d;g' -i -e "s/\(# LIST BEGIN\s*\n\).*\(# LIST END\s*\n\)/\1$series_list\2/ig" index.org
+  cd ..
+}
+
+gen_indexes
+
+
+mkdir -p html
+cp style.css html/
+emacs -Q --script build-site.el
